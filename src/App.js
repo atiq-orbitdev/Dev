12effd2c2baab702151213bc40a00f0cf0a1a482
Atiq <95067358+atiq-orbitdev@users.noreply.github.com>
2025-04-12 12:26:35 +0000

    meunu item added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import ToDoGrid from './components/ToDoGrid';
 function App() {
   return (
     <div className="App">
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">To Do Items</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
       <ToDoGrid />
     </div>
   );
